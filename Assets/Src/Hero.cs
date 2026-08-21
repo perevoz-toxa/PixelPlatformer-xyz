52b9ff974855ee55057c5aa8711289bf8bd8011d
Tony Perevozchikov <perevoz.toxa@gmail.com>
2026-08-21 17:10:18 +0500

    feat: initial project structure - sprite-based hero, coins, environment
    - Hero with Rigidbody2D movement and jump mechanics
    - Hero input reader (GamePad/Keyboard)
    - Ground check system using LayerMask
    - Prefabs: Hero, Coins (Gold/Silver), Environment (Palm, Water), Props (Spikes)
    - Input System setup
    - LayerCheck for grounded detection
    
    Next: migrate from sprite-based to tilemap-based environment

---
+++ Assets/Src/Hero.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Hero : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _jumpSpeed;
+    [SerializeField] private LayerCheck _groundCheck;
+
+    private Rigidbody2D _rigidbody;
+    private Vector2 _direction;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    public void SetDirection(Vector2 direction)
+    {
+        _direction = direction;
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.velocity = new Vector2(
+            _direction.x * _speed,
+            _rigidbody.velocity.y
+        );
+
+        var isJumping = _direction.y > 0;
+        if (isJumping)
+        {
+            if (IsGrounded())
+            {
+                _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
+            }
+        }
+        else if (_rigidbody.velocity.y > 0)
+        {
+            _rigidbody.velocity = new Vector2(
+              _direction.x * _speed,
+              _rigidbody.velocity.y * 0.5f
+          );
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return _groundCheck.IsTouchingLayer;
+    }
+
+    public void SaySomething()
+    {
+        Debug.Log("I just say something. Hi! For example.");
+    }
+}
---
+++ Assets/Src/HeroInputReader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Hero))]
+public class HeroInputReader : MonoBehaviour
+{
+    [SerializeField] private Hero _hero;
+
+    private void Update()
+    {
+
+    }
+
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        var direction = context.ReadValue<Vector2>();
+        _hero.SetDirection(direction);
+    }
+
+    public void OnSaySomething(InputAction.CallbackContext context)
+    {
+        if (context.canceled)
+        {
+            _hero.SaySomething();
+        }
+    }
+}
