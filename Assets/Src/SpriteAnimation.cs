75da3249e9045738ae9d68ef5a52a0c2a8e5d137
Tony Perevozchikov <perevoz.toxa@gmail.com>
2026-08-27 11:25:09 +0500

    feat: level layout, sprite animation enhancements, environment textures
    - SpriteAnimation: additional features from homework
    - Level1: arranged level layout
    - New environment textures: clouds, chain, background
    - Updated coin and environment prefabs

--- Assets/Src/SpriteAnimation.cs
+++ Assets/Src/SpriteAnimation.cs
@@ -1,19 +1,35 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
+using System.Collections.Generic;
 
 [RequireComponent(typeof(SpriteRenderer))]
+
+[System.Serializable]
+public class SpriteClip
+{
+    [SerializeField] private string _name;
+    [SerializeField] private Sprite[] _sprites;
+    [SerializeField] private bool _loop;
+    [SerializeField] private bool _allowNext;
+
+    public string Name => _name;
+    public Sprite[] Sprites => _sprites;
+    public bool Loop => _loop;
+    public bool AllowNext => _allowNext;
+}
 
 public class SpriteAnimation : MonoBehaviour
 {
     [SerializeField] private int _frameRate;
-    [SerializeField] private bool _loop;
-    [SerializeField] private Sprite[] _sprites;
+    [SerializeField] private List<SpriteClip> _clips;
+    [SerializeField] private string _currentClipName;
     [SerializeField] private UnityEvent _onComplete;
 
     private SpriteRenderer _renderer;
     private float _secondsPerFrame;
     private int _currentSpriteIndex;
     private float _nextFrameTime;
+    private SpriteClip _currentClip;
 
     private bool _isPlaying = true;
 
@@ -22,29 +38,54 @@ public class SpriteAnimation : MonoBehaviour
         _renderer = GetComponent<SpriteRenderer>();
         _secondsPerFrame = 1f / _frameRate;
         _nextFrameTime = Time.time + _secondsPerFrame;
+        SetClip(_currentClipName);
     }
 
     private void Update()
     {
         if (!_isPlaying || _nextFrameTime > Time.time) return;
 
-        if (_currentSpriteIndex >= _sprites.Length)
+        if (_currentSpriteIndex >= _currentClip.Sprites.Length)
         {
-            if (_loop)
+            if (_currentClip.Loop)
             {
                 _currentSpriteIndex = 0;
             }
             else
             {
-                _isPlaying = false;
-                _onComplete?.Invoke();
-                return;
+                if (_currentClip.AllowNext)
+                {
+                    int currentIndex = _clips.IndexOf(_currentClip);
+                    if (currentIndex >= 0 && currentIndex < _clips.Count - 1)
+                    {
+                        SetClip(_clips[currentIndex + 1].Name);
+                    }
+                }
+                else
+                {
+                    _isPlaying = false;
+                    _onComplete?.Invoke();
+                    return;
+                }
             }
-
         }
 
-        _renderer.sprite = _sprites[_currentSpriteIndex];
+        _renderer.sprite = _currentClip.Sprites[_currentSpriteIndex];
         _nextFrameTime += _secondsPerFrame;
         _currentSpriteIndex++;
+    }
+
+    public void SetClip(string clipName)
+    {
+        _currentClip = _clips.Find(c => c.Name == clipName);
+        if (_currentClip == null)
+        {
+            Debug.LogWarning($"Clip '{clipName}' not found!");
+            return;
+        }
+        _currentSpriteIndex = 0;
+        _isPlaying = true;
+        _nextFrameTime = Time.time + _secondsPerFrame;
+        _renderer.sprite = _currentClip.Sprites[0];
     }
 }
