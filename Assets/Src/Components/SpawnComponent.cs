1869487b56c8bb95852852236a341708eba17edf
Tony Perevozchikov <perevoz.toxa@gmail.com>
2026-09-02 13:02:47 +0500

    refactor: hero prefab restructure, foot step particles
    - Hero.prefab: restructured
    - FootSteps.prefab: new particle effect for hero footsteps
    - DustParticles: renamed folder (removed space), reorganized under Run/
    - SpawnComponent: reusable spawn system with [ContextMenu]

---
+++ Assets/Src/Components/SpawnComponent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class SpawnComponent : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private GameObject _prefab;
+
+    [ContextMenu("Spawn")]
+    public void Spawn()
+    {
+        Instantiate(_prefab, _target.position, Quaternion.identity);
+    }
+}
