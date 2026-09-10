671a3a2ae1750df1d58a2df5705280417dd03f3f
Tony Perevozchikov <perevoz.toxa@gmail.com>
2026-09-10 14:02:55 +0500

    feat: exit door and level transition
    - ExitLevelComponent: loads next scene via SceneManager
    - ExitDoor.prefab: new exit door prefab
    - Door textures added to Environment folder
    - Updated Level1 and Level2 layouts

---
+++ Assets/Src/Components/ExitLevelComponent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace Assets.Src.Components
+{
+    public class ExitLevelComponent : MonoBehaviour
+    {
+        [SerializeField] private string _nextSceneName;
+        public void Exit()
+        {
+            SceneManager.LoadScene(_nextSceneName);
+        }
+    }
+}
