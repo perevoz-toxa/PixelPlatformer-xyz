754454252046f9007cc1c54c68e62939c114c1b6
Tony Perevozchikov <perevoz.toxa@gmail.com>
2026-09-14 17:01:01 +0500

    refactor: remove unused imports

--- Assets/Src/Components/TeleportComponent.cs
+++ Assets/Src/Components/TeleportComponent.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UIElements;
 
 namespace Assets.Src.Components
 {
