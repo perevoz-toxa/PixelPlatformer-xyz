bd138b2af56279d814c5393b8d53dbd8bb7abe50
Tony Perevozchikov <perevoz.toxa@gmail.com>
2026-09-14 17:04:07 +0500

    feat: camera state control, target show, timed events
    - CameraStateController: move camera to target position with animation
    - ShowTargetComponent: show target with delay, auto-move back
    - TimerComponent: timed events with delay and OnTimesUp
    - CutScenes: new cutscene prefab

---
+++ Assets/Src/Components/ShowTargetComponent.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Assets.Src.Components
+{
+    public class ShowTargetComponent : MonoBehaviour
+    {
+        [SerializeField] private Transform _target;
+        [SerializeField] private CameraStateController _controller;
+        [SerializeField] private float _delay = 1f;
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (_controller == null)
+                _controller = FindObjectOfType<CameraStateController>();
+        }
+#endif
+        public void ShowTarget()
+        {
+            _controller.SetPosition(_target.position);
+            _controller.SetState(true);
+            Invoke(nameof(MoveBack), _delay);
+        }
+
+        private void MoveBack()
+        {
+            _controller.SetState(false);
+        }
+    }
+}
